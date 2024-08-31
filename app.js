960467aaab9fe92bc7dc6cfd4a773f7830f5cec3
solugsm <fabioarielarguello@hotmail.com>
2024-08-30 23:45:36 -0300

    argumax

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+//motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views',__dirname + "/views");
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+
+app.get('/', (req, res) => {
+    res.render('index',{titulo: "mi titulo dinámico"})
+})
+
+app.get('/servicios', (req, res) => {
+    res.render("servicios", {tituloservicios: "Este es un mensaje dinámico de servicios"})
+})
+
+app.use((req, res, next) => {
+    res.status(404).render("404", {
+        titulo: "404",
+        descripcion: "Titulo del sitio web"
+    })
+})
+
+
+
+app.listen(port, () => {
+    console.log('servidor a su servicio en el puerto', port)
+})
